5567685a60e4ff36e02f5e1fab5da3d08b245c9a
Annie Tang <annietang@berkeley.edu>
2018-05-19 08:01:00 -0700

    added admin servlet and jsp file, and modified web xml

---
+++ src/main/java/codeu/controller/AdminServlet.java
@@ -0,0 +1,24 @@
+package codeu.controller;
+
+import codeu.model.data.User;
+import codeu.model.store.basic.UserStore;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdmindServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+        
+  }
+
+  // @Override
+  // public void doPost(HttpServletRequest request, HttpServletResponse response)
+  //     throws IOException, ServletException {
+  // }
+
+}
