cd7db5d1251588ce8ce520c39dd1af1a3425a31f
Chibuikem Ohanele <chibuikemohanele@Chibuikems-MacBook-Pro.local>
2018-05-20 18:33:44 -0700

    checks to see if the user is looking at his or her own profile

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -4,12 +4,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+//import codeu.model.data.User;
+//import codeu.model.store.basic.UserStore;
 
 public class ProfileServlet extends HttpServlet
 {
+    /** Store class that gives access to Users. */
+    //private UserStore userStore;
+
+    //Forwards the request to the profile.jsp to create the profile page
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws IOException, ServletException {
-        request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+    throws IOException, ServletException 
+    {
+        String username = request.getParameter("username");
+
+        if (username.equals(request.getSession().getAttribute("username"))) 
+        {
+            request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+            return;
+        }
     }
+
 }
